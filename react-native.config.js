4b7a2bec52f67111c0eef5e96da8e847e24c342a
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2021-01-04 02:24:02 +0600

    fix android

--- react-native.config.js
+++ react-native.config.js
@@ -7,11 +7,13 @@ module.exports = {
     'react-native-fetch-blob': {
       platforms: {
         android: null, // disable Android platform, other platforms will still autolink if provided
+        ios: null,
       },
     },
     'react-native-exit-app': {
       platforms: {
         android: null,
+        ios: null,
       }
     }
   },
